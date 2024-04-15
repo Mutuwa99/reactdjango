9dd599c15421dfc85ccd37af076857378691a2e9
mutuwa99 <mutuwanoble@MUTUWAs-MacBook-Air.local>
2024-04-15 12:14:47 +0200

    task API

--- frontend1/src/pages/Create.js
+++ frontend1/src/pages/Create.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // import { useLocation } from 'react-router-dom';
-import Menu from './componenets/menu';
+import Menu from './Menu';
 // import { showToast } from './toast';
 import $ from 'jquery';
 
