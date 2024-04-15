f3300a5a2dd26dade0e9be753dba497143d9d490
mutuwa99 <mutuwanoble@MUTUWAs-MacBook-Air.local>
2024-04-15 13:34:38 +0200

    toast

---
+++ frontend1/src/pages/toast.js
@@ -0,0 +1,11 @@
+import iziToast from 'izitoast';
+
+
+export const showToast = (title, message) => {
+    iziToast.show({
+        title: title,
+        message: message,
+        color: 'green', 
+        position: 'topRight',
+    });
+};
